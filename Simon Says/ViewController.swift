065da2bf09d263a3158553c9720785163573d808
Ievgeniia Pavliuchyk <ievgeniia.pavliuchyk@gmail.com>
2019-02-20 23:53:36 +0100

    Initial Commit

---
+++ Simon Says/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simon Says
+//
+//  Created by Ievgeniia Pavliuchyk on 20/02/2019.
+//  Copyright © 2019 Ievgeniia Pavliuchyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
